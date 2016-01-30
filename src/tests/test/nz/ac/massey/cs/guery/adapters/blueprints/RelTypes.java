f144edbb824e3ef5cbda462276788db2c9848efe
Lars Schuetze <dev@larsschuetze.de>
2016-01-30 15:31:19 +0100

    Remove single test folder since the tests have been splitted

--- src/tests/test/nz/ac/massey/cs/guery/adapters/blueprints/RelTypes.java
+++
@@ -1,8 +0,0 @@
-package test.nz.ac.massey.cs.guery.adapters.blueprints;
-
-import org.neo4j.graphdb.RelationshipType;
-
-public enum RelTypes implements RelationshipType {
-	EXTENDS, IMPLEMENTS, USES
-
-}
